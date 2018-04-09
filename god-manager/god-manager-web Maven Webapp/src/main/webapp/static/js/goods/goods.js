5fa5a569cc93a8cd6cc5b486801a6c9ec4602347
yanlaijin <yanlaijin@qq.com>
2018-04-09 22:34:04 +0800

    update

--- god-manager/god-manager-web Maven Webapp/src/main/webapp/static/js/goods/goods.js
+++ god-manager/god-manager-web Maven Webapp/src/main/webapp/static/js/goods/goods.js
@@ -1,2 +1,9 @@
 $(function () {
+    var parentItem = $("#parent_ck");
+    parentItem.change(function () {
+        var children = $("[name='child_ck']");
+        children.each(function () {
+            $(this).prop("checked", parentItem.prop("checked"));
+        })
+    })
 })
--- god-manager/god-manager-web Maven Webapp/src/main/webapp/static/js/index.js
+++ god-manager/god-manager-web Maven Webapp/src/main/webapp/static/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+	//菜单选中状态
 	var menuList = $("[data-url]");
 	var iframe = $("#iframe");
 	var menu = null;
@@ -6,7 +7,17 @@ $(function () {
 		menu = $(this);
 		menu.click(function() {
 			$(".active").removeClass("active");
-			menu.parent("li").addClass("active");
+            var childrenMenu = menu.parent("li");
+            childrenMenu.addClass("active");
+			var parentMenu = childrenMenu.parent("ul");
+            var htmlparam = null;
+            if (parentMenu) {
+                htmlparam = "<li><i class='ace-icon fa "+parentMenu.data("cls")+" home-icon'></i> <a href='#'>"+menu.text()+"</a></li>"
+            } else {
+                htmlparam = "<li><i class='ace-icon fa "+childrenMenu.data("cls")+" home-icon'></i> <a href='#'>menu.text()</a></li>"
+            }
+            var labels = $("#breadcrumb");
+            labels.html(htmlparam);
 			iframe.attr("src",$(this).data("url"));
 		})
 	})
